5a8d26df40ce9072a572bd8f95bafc32803475e9
SWATI GARJE <56024644+swati1547@users.noreply.github.com>
2023-02-02 17:51:53 +0530

    project update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-
 import { createRoot } from 'react-dom/client'
-
 import './index.css';
 import App from "./App.jsx"
 import {BrowserRouter} from "react-router-dom";
